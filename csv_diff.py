219ffb9046aa33bd9fc8904df10b6d852d353efb
ianmcfarlane <ian.mcfarlane@elementalled.com>
2017-02-21 10:39:24 -0800

    Python CSV diff script

---
+++ csv_diff.py
@@ -0,0 +1,15 @@
+import csv
+with open('/Users/ian/Desktop/PartCategorization022117.csv', 'rb') as csvfile1:
+    with open ("/Users/ian/Desktop/UpdatedPartCategorization.csv", "rb") as csvfile2:
+        reader1 = csv.reader(csvfile1)
+        reader2 = csv.reader(csvfile2)
+        rowsCurrent = {row[0]:row for row in reader1} #file1 row dict
+        rowsUpdated = [row for row in reader2] #file2 row list
+        diffList = []
+        for row in rowsUpdated:
+            if row[0] in rowsCurrent and rowsCurrent[row[0]] != row:
+                diffList.append(row)
+        print len(diffList)
+        with open('/Users/ian/Desktop/diffedCSV.csv', 'wb') as diffFile:
+            writer = csv.writer(diffFile)
+            writer.writerows(diffList)
